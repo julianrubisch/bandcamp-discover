0778402d6422e7ac434ba336882be8e734c79076
Julian Rubisch <julian@julianrubisch.at>
2025-02-21 14:53:16 +0100

    feat: Return label name and location

--- lib/bandcamp-discover/scrapers/label.rb
+++ lib/bandcamp-discover/scrapers/label.rb
@@ -10,13 +10,18 @@ module BandcampDiscover
           page.goto(@url)
           bio_container = page.wait_for_selector("#bio-container")
           bio_text = bio_container.query_selector("#bio-text")
+
+          band_name_location_container = page.wait_for_selector("#band-name-location")
+          name = band_name_location_container.query_selector(".title").inner_text
+          location = band_name_location_container.query_selector(".location").inner_text
+
           if bio_text&.inner_html =~ /label|platform/
             return Sync do
               music_tags = Scrapers::Music.new(url: "#{@url}/music", browser: @browser, max_tasks: @max_tasks).scrape
 
               puts "done scraping #{@url}"
 
-              [@url, music_tags]
+              [@url, name, location, music_tags&.compact]
             end
           else
             puts "not a label: #{@url}"
